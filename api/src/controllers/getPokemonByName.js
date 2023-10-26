e12446a349979b3eef85d53d18566de62069e758
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-26 20:28:36 -0300

    GetPokemonByName modified return Pokémons by name from api and DB

--- api/src/controllers/getPokemonByName.js
+++ api/src/controllers/getPokemonByName.js
@@ -1,16 +1,27 @@
-// const { Pokemon } = require("../db");
+const { Pokemon } = require("../db");
 const axios = require('axios')
+const { Op } = require("sequelize");
 const URL = `http://pokeapi.co/api/v2/pokemon`
 
 const getPokemonByName = async (req, res) => {
     try {
         const { name } = req.query;
-        const lowercaseName = name.toLowerCase();
+        const lowercaseName = name.toLowerCase(); // Búsqueda independiente de mayúsculas o minúsculas;
+
+        const pokemonFromDB = await Pokemon.findAll({ // Buscamos el Pokémon en la base de datos;
+            where: {
+                name: {
+                    [Op.iLike]: `%${lowercaseName}%`, 
+                },
+            },
+        });
+
         const response = await axios.get(`${URL}/${lowercaseName}`);
 
-        const pokemonData = response.data;
-            
-            const pokemonByName = {
+        if (response.data) {
+            const pokemonData = response.data;
+
+            const pokemonFromApi = { // Buscamos el Pokémon en la api;
                 id: pokemonData.id,
                 name: pokemonData.name,
                 image: pokemonData.sprites.front_default,
@@ -19,8 +30,11 @@ const getPokemonByName = async (req, res) => {
                 defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
                 types: pokemonData.types.map((type) => type.type.name),
             };
-            return res.status(200).json(pokemonByName);
-        } catch (error) {
+
+            const allPokemons = [...pokemonFromDB, pokemonFromApi];
+            return res.status(200).json(allPokemons);
+        }
+    } catch (error) {
         return res.status(404).json({ message: "Pokemon no encontrado" });
     }
 }
