2c6f8c4cb73a2d7afd7b5fc6255674f82c2f0b00
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-26 12:56:03 -0300

    GetPokemonById modified search Pokemon in DB

--- api/src/controllers/getPokemonById.js
+++ api/src/controllers/getPokemonById.js
@@ -1,4 +1,4 @@
-// const { Pokemon } = require("../db");
+const { Pokemon } = require("../db");
 const axios = require('axios')
 const URL = `http://pokeapi.co/api/v2/pokemon`
 
@@ -6,29 +6,30 @@ const getPokemonById = async (req, res) => {
     try {
         const { id } = req.params;
 
-        // const pokemonFromDB = await Pokemon.findOne({ // Buscamos el Pokémon en la base de datos;
-        //     where: { id: id },
-        // });
-
-        // if (pokemonFromDB) {
-        //     return res.status(200).json(pokemonFromDB);
-        // }
+        if (id.length > 4) {
+            const pokemonFromDB = await Pokemon.findOne({ // Buscamos el Pokémon en la base de datos;
+                where: { id: id },
+            });
+            if (pokemonFromDB.id) {
+                return res.status(200).json(pokemonFromDB);
+            }
+        }
 
         const response = await axios.get(`${URL}/${id}`);
         const pokemonData = response.data;
 
-            const pokemonFromApi = {
-                id: pokemonData.id,
-                name: pokemonData.name,
-                image: pokemonData.sprites.front_default,
-                hp: pokemonData.stats.find((stat) => stat.stat.name === "hp").base_stat,
-                attack: pokemonData.stats.find((stat) => stat.stat.name === "attack").base_stat,
-                defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
-                types: pokemonData.types.map((type) => type.type.name),
-            };
-            return res.status(200).json(pokemonFromApi);
+        const pokemonFromApi = {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            image: pokemonData.sprites.front_default,
+            hp: pokemonData.stats.find((stat) => stat.stat.name === "hp").base_stat,
+            attack: pokemonData.stats.find((stat) => stat.stat.name === "attack").base_stat,
+            defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
+            types: pokemonData.types.map((type) => type.type.name),
+        };
+        return res.status(200).json(pokemonFromApi);
     } catch (error) {
-        return res.status(404).json({ message: "Pokemon no encontrado" });
+        return res.status(404).json({ error: error.message });
     }
 }
 
