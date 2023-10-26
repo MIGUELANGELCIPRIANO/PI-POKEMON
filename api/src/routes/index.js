208a5451b730b44b0a3d0fa1639b0666f848c98b
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-26 20:32:50 -0300

    RoutesIndex modified add routes getPokemonByName, getPokemonById, getAllPokemons and postPokemon

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,8 +3,8 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 const { getAllPokemons } = require('../controllers/getAllPokemons');
 const { getPokemonById } = require('../controllers/getPokemonById');
-// const { getPokemonByName } = require('../controllers/getPokemonByName');
-// const { postPokemon } = require('../controllers/postPokemon');
+const { getPokemonByName } = require('../controllers/getPokemonByName');
+const { postPokemon } = require('../controllers/postPokemon');
 // const { getAllTypes } = require('../controllers/getAllTypes');
 
 const router = Router();
@@ -12,13 +12,13 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/pokemon', getAllPokemons); 
+router.get('/pokemon/name', getPokemonByName);
 
 router.get('/pokemon/:id', getPokemonById);
 
-// router.get('pokemon/:name', getPokemonByName);
+router.get('/pokemon', getAllPokemons); 
 
-// router.post('pokemon', postPokemon);
+router.post('/pokemon', postPokemon);
 
 // router.get('type', getAllTypes);
 
