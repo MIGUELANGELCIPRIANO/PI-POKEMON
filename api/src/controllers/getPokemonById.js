4fae0b878aa8a8445837951027b95d4a926eaeb8
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-26 00:04:37 -0300

    GetPokemonById modified

--- api/src/controllers/getPokemonById.js
+++ api/src/controllers/getPokemonById.js
@@ -28,7 +28,7 @@ const getPokemonById = async (req, res) => {
             };
             return res.status(200).json(pokemonFromApi);
     } catch (error) {
-        return res.status(500).send(error.message);
+        return res.status(404).json({ message: "Pokemon no encontrado" });
     }
 }
 
