dea585567058c08218b52b54c8c705f62521b3ff
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-27 16:23:03 -0300

    GetPokemonByName modified return Pokémons by name from api and DB

--- api/src/controllers/getPokemonByName.js
+++ api/src/controllers/getPokemonByName.js
@@ -1,43 +1,53 @@
 const { Pokemon } = require("../db");
-const axios = require('axios')
+const axios = require('axios');
 const { Op } = require("sequelize");
-const URL = `http://pokeapi.co/api/v2/pokemon`
+const URL = `http://pokeapi.co/api/v2/pokemon`;
 
 const getPokemonByName = async (req, res) => {
     try {
         const { name } = req.query;
-        const lowercaseName = name.toLowerCase(); // Búsqueda independiente de mayúsculas o minúsculas;
+        const lowercaseName = name.toLowerCase();
 
-        const pokemonFromDB = await Pokemon.findAll({ // Buscamos el Pokémon en la base de datos;
+        const pokemonsFromDB = await Pokemon.findAll({ // Buscamos el Pokémon en la base de datos;
             where: {
                 name: {
-                    [Op.iLike]: `%${lowercaseName}%`, 
-                },
-            },
+                    [Op.iLike]: `%${lowercaseName}%`
+                    // [Op.iLike] busca registros cuyo campo coincida con el valor proporcionado sin distinción entre mayúsculas o minúsculas;
+                }
+            }
         });
 
-        const response = await axios.get(`${URL}/${lowercaseName}`);
+        const pokemonsFromApi = []; // Buscamos el Pokémon en la API;
+        try {
+            const response = await axios.get(`${URL}/${lowercaseName}`);
 
-        if (response.data) {
-            const pokemonData = response.data;
+            if (response.data) {
+                const pokemonData = response.data;
 
-            const pokemonFromApi = { // Buscamos el Pokémon en la api;
-                id: pokemonData.id,
-                name: pokemonData.name,
-                image: pokemonData.sprites.front_default,
-                hp: pokemonData.stats.find((stat) => stat.stat.name === "hp").base_stat,
-                attack: pokemonData.stats.find((stat) => stat.stat.name === "attack").base_stat,
-                defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
-                types: pokemonData.types.map((type) => type.type.name),
-            };
+                const pokemonFound = {
+                    id: pokemonData.id,
+                    name: pokemonData.name,
+                    image: pokemonData.sprites.front_default,
+                    hp: pokemonData.stats.find((stat) => stat.stat.name === "hp").base_stat,
+                    attack: pokemonData.stats.find((stat) => stat.stat.name === "attack").base_stat,
+                    defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
+                    types: pokemonData.types.map((type) => type.type.name),
+                };
+                pokemonsFromApi.push(pokemonFound);
+            }
+        } catch (error) { }
 
-            const allPokemons = [...pokemonFromDB, pokemonFromApi];
-            return res.status(200).json(allPokemons);
+        const allPokemons = [...pokemonsFromDB, ...pokemonsFromApi];
+
+        if (allPokemons.length === 0) {
+            return res.status(404).json({ message: "Pokemon Not Found" });
         }
+        return res.status(200).json(allPokemons);
+
     } catch (error) {
-        return res.status(404).json({ message: "Pokemon no encontrado" });
+        return res.status(500).json({ error: error.message });
     }
-}
+};
 
 module.exports = {
     getPokemonByName
