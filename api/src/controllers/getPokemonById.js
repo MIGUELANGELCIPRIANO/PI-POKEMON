e8f74192da156b3d58b4a353b66e7b95839e78f5
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-29 21:09:52 -0300

    GetPokemonById comments added

--- api/src/controllers/getPokemonById.js
+++ api/src/controllers/getPokemonById.js
@@ -4,23 +4,23 @@ const URL = `http://pokeapi.co/api/v2/pokemon`;
 
 const getPokemonById = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { id } = req.params; 
 
         if (id.length > 4) {
-            const pokemonFromDB = await Pokemon.findOne({ // Buscamos si existe el Pokémon en la base de datos;
+            const pokemonFromDB = await Pokemon.findOne({ // Verificar si existe el Pokémon en la base de datos;
                 where: { id: id },
             });
-            if (pokemonFromDB.id) {
-                return res.status(200).json(pokemonFromDB);
+            if (pokemonFromDB) {
+                return res.status(200).json(pokemonFromDB); // Retorna el Pokémon de la base de datos;
             }
         };
 
-        const response = await axios.get(`${URL}/${id}`);
+        const response = await axios.get(`${URL}/${id}`); // Solicitud a la API para obtener el Pokémon por id;
 
         if (response.data) {
             const pokemonData = response.data;
 
-            const pokemonFromApi = { // Caso contrario buscamos si existe el Pokémon en la API;
+            const pokemonFromApi = {
                 id: pokemonData.id,
                 name: pokemonData.name,
                 image: pokemonData.sprites.front_default,
@@ -29,9 +29,9 @@ const getPokemonById = async (req, res) => {
                 defense: pokemonData.stats.find((stat) => stat.stat.name === "defense").base_stat,
                 types: pokemonData.types.map((type) => type.type.name),
             };
-            return res.status(200).json(pokemonFromApi);
+            return res.status(200).json(pokemonFromApi); // Retorna el Pokémon de la API;
         }
-        return res.status(404).json({ message: "Pokemon Not Found" });
+        return res.status(404).json({ message: "Pokémon not found" });
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
