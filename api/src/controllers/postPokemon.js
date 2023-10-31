30567ecfbc7e930e147f7a7cfee15b739377839e
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-10-31 17:39:30 -0300

    List of types in PostPokemon

--- api/src/controllers/postPokemon.js
+++ api/src/controllers/postPokemon.js
@@ -1,14 +1,11 @@
 const { Pokemon, Type } = require('../db');
-const axios = require('axios')
-const URL = `http://pokeapi.co/api/v2/pokemon`
-
 
 const postPokemon = async (req, res) => {
   try {
     const { name, image, hp, attack, defense, types } = req.body;
 
     if (!name || !image || !hp || !attack || !defense || !types ) {
-      return res.status(400).json({ message: "Faltan datos o tipos insuficientes" });
+      return res.status(404).json({ message: "Insufficient data or types" });
     }
 
     const newPokemon = await Pokemon.create({ // Crear el Pokémon en la base de datos;
@@ -16,22 +13,22 @@ const postPokemon = async (req, res) => {
       image,
       hp: parseInt(hp),
       attack: parseInt(attack),
-      defense: parseInt(defense),
+      defense: parseInt(defense)
     });
 
-    // const typeRecords = await Type.findAll({ // Buscar y relacionar los tipos con el Pokémon;
-    //   where: {
-    //     name: types,
-    //   },
-    // });
+    const typeRecords = await Type.findAll({ // Verificar si existen los Types en la base de datos;
+      where: {
+        name: types
+      },
+    });
 
-    // if (typeRecords.length === types.length) {
+    if (typeRecords.length === types.length) { // Verificar si los Types encontrados coinciden con types;
 
-    //   await newPokemon.setTypes(typeRecords); // Relacionar los tipos encontrados con el nuevo Pokémon;
+      await newPokemon.setTypes(typeRecords); // Relacionar los Types encontrados con el nuevo Pokémon;
 
-      return res.status(201).json(newPokemon);
-    // }
-    // return res.status(400).json({ message: "Alguno de los tipos especificados no existe" });
+      return res.status(200).json(newPokemon); // Retorna el Pokémon creado de la base de datos;
+    }
+    return res.status(404).json({ message: "One of the specified types does not exist" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
