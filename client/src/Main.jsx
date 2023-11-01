b8f5cae46dec0f7fa486029b6e79aa2660380169
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-01 18:26:54 -0300

    Modified ReactDOM, importing Provider, store and BrowserRouter

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,15 +1,19 @@
+import './Main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>   
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
