90f17d13fe1b6a7c0763cef4f68ff3f49f19bbe7
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-01 18:25:58 -0300

    Landing, home and detail routes added to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './page/homePage/Home';
+import Landing from './page/landingPage/Landing';
+import Detail from './page/detailPage/Detail';
 
 function App() {
   return (
     <div className="App">
-      <h1>HENRY POKÉMON</h1>
+      <Routes>
+        <Route path='' element={<Landing/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/detail/:id' element={<Detail/>}></Route>
+      </Routes>
     </div>
   );
 }
