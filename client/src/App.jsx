3aa54ccb44ee008f4904ff215bbd656c50a677c9
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-02 14:58:21 -0300

    `navBar` added to `App`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './page/homePage/Home';
 import Landing from './page/landingPage/Landing';
 import Detail from './page/detailPage/Detail';
+import NavBar from './components/navbar/NavBar';
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path='' element={<Landing/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
