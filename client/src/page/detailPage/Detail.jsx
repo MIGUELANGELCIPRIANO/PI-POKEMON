ca796aa2abf7a05dd5feaefb71c4ffbc017f1bfd
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-02 02:26:39 -0300

    DetailPage modified in the page folder

--- client/src/page/detailPage/Detail.jsx
+++ client/src/page/detailPage/Detail.jsx
@@ -1,28 +1,26 @@
 import './Detail.css';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPokemonDetail } from '../../redux/actions';
 
 const Detail = () => {
-    const params = useParams();
-    const dispatch = useDispatch();
-    const { pokemonDetail } = useSelector((state) => state);
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { pokemonDetail } = useSelector((state) => state);
 
-    useEffect(() => {
-        dispatch(getPokemonDetail(params?.id)); // Se monta el componente realizando un dispatch a la action;
-      }, [params?.id]);
+  useEffect(() => {
+    dispatch(getPokemonDetail(params?.id)); // Se monta el componente realizando un dispatch a la action;
+  }, [params?.id]);
 
   return (
     <div className='Detail'>
-        <h2>{pokemonDetail.name}</h2>
-        <img src={pokemonDetail?.image} alt={pokemonDetail?.name} />
-        <p>{pokemonDetail?.hp}</p>
-        <p>{pokemonDetail?.attack}</p>
-        <p>{pokemonDetail?.defense}</p>
-        <p>{pokemonDetail?.speed}</p>
-        <p>{pokemonDetail?.height}</p>
-        <p>{pokemonDetail?.weight}</p>
-        <p>{pokemonDetail?.types}</p>
+      <h2>{pokemonDetail?.name}</h2>
+      <img src={pokemonDetail?.image} alt={pokemonDetail?.name} />
+      <p>hp: {pokemonDetail?.hp}</p>
+      <p>attack: {pokemonDetail?.attack}</p>
+      <p>defense: {pokemonDetail?.defense}</p>
+      <p>{pokemonDetail?.types}</p>
     </div>
   );
 }
