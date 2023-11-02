e40bbec3d626c0e42970d768b95c22addcbd030e
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-02 05:20:07 -0300

    `GetAllTypes` code syntax

--- api/src/controllers/getAllTypes.js
+++ api/src/controllers/getAllTypes.js
@@ -7,13 +7,14 @@ const getAllTypes = async (req, res) => {
     const response = await axios.get(`${URL}`); // Solicitud a la API para obtener la lista de Types;
     const typesFromAPI = response.data.results.map((result) => result.name);
 
-    const typesFromDB = await Type.findAll(); 
+    const typesFromDB = await Type.findAll(); // `findAll` consulta y recupera datos desde una base de datos local (como base de datos SQL) de acuerdo con los criterios de búsqueda proporcionados;
 
     if (typesFromDB.length === 0) { // Verificar si la base de datos está vacía;
-      await Type.bulkCreate(typesFromAPI.map((name) => ({ name }))); // Inserta los Types de la API en la base de datos;
+      await Type.bulkCreate(typesFromAPI.map((name) => ({ name }))); // `bulkCreate` inserta múltiples registros dentro de una base de datos local (como base de datos SQL) de manera útil frente a relaciones many-to-many;
     }
 
-    return res.status(200).json(typesFromDB); // Retorna la lista de Types de la base de datos;
+    return res.status(200).json(typesFromDB);
+
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
