92182da09d0aec81c5579a3413d50e6e4e3281ac
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-03 15:29:25 -0300

    Action getAllTypes added to `actions`

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,32 +1,45 @@
-import { GET_ALL_POKEMONS, GET_POKEMON_DETAIL, CLEAN_POKEMON_DETAIL, } from "./actionsTypes"
+import { GET_ALL_POKEMONS, GET_POKEMON_DETAIL, CLEAN_POKEMON_DETAIL, GET_ALL_TYPES } from "./actionsTypes"
 import axios from "axios";
 
-const URL = `http://localhost:3001/pokemons`;
+const URLPokemon = `http://localhost:3001/pokemons`;
+const URLTypes = `http://localhost:3001/types`;
 
 export const getAllPokemons = () => {
     return async (dispatch) => {
         try {
-            const response = await axios(`${URL}`);
+            const response = await axios(`${URLPokemon}`);
             const data = response.data;
             dispatch({ type: GET_ALL_POKEMONS, payload: data });
         } catch (error) {
             throw new Error(error.message);
         }
-    };
+    }
 }
 
 export const getPokemonDetail = (id) => {
     return async (dispatch) => {
         try {
-            const response = await axios(`${URL}/${id}`);
+            const response = await axios(`${URLPokemon}/${id}`);
             const data = response.data;
             dispatch({ type: GET_POKEMON_DETAIL, payload: data });
         } catch (error) {
             throw new Error(error.message);
         }
-    };
+    }
 }
 
 export const cleanPokemonDetail = () => {
     return { type: CLEAN_POKEMON_DETAIL }
+}
+
+export const getAllTypes = () => {
+    return async (dispatch) => {
+        try {
+            const response = await axios(`${URLTypes}`);
+            const data = response.data;
+            dispatch({ type: GET_ALL_TYPES, payload: data });
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
 }
