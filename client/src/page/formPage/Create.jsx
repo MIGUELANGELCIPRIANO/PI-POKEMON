7d3f7dfbe601360b5121fb1c33c9571177b70efa
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-05 13:52:34 -0300

    postPokemon imported from `actions`, useNavigate() and create button disabled

--- client/src/page/formPage/Create.jsx
+++ client/src/page/formPage/Create.jsx
@@ -1,12 +1,12 @@
 import './Create.css';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import validations from './validations';
-import { getAllTypes } from '../../redux/actions';
-import Button from '../../components/button/Button';
+import { getAllTypes, postPokemon } from '../../redux/actions';
 
 const Create = () => {
-  const [input, setInput] = useState({ // Rastrear los valores ingresados por campo;
+  const [input, setInput] = useState({
     name: '',
     image: '',
     hp: '',
@@ -15,116 +15,114 @@ const Create = () => {
     types: '',
   });
 
+
   const [errors, setErrors] = useState({});
 
-  const dispatch = useDispatch();
-  const allTypes = useSelector((state) => state.allTypes); // Obtener los types de estado;
-
   const handleChange = (event) => {
-    const { name, value } = event.target;
-    if (name === 'types') {
-      setInput({
-        ...input,
-        types: [... input.types, value]
-      })
-      return
-    }
     setInput({
       ...input,
-      [name]: value,
+      [event.target.name]: event.target.value,
     });
     setErrors(validations(input));
   }
- console.log(input);
 
-    useEffect(() => { // Cargar los types cuando se monta el componente;
-      dispatch(getAllTypes());
-    }, []);
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // dispatch(createPokemon(input));
-    }
+  const dispatch = useDispatch();
+  const allTypes = useSelector((state) => state.allTypes);
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <h1>Create your Pokémon!</h1>
+  useEffect(() => {
+    dispatch(getAllTypes());
+  }, [dispatch]);
 
-        <label htmlFor="name">Name: </label>
-        <input
-          type="text"
-          name="name"
-          value={input.name}
-          onBlur={handleChange}
-          onChange={handleChange}
-        />
-        {errors.name && <p>{errors.name}</p>}
-        <br />
 
-        <label htmlFor="image">Image URL: </label>
-        <input
-          type="text"
-          name="image"
-          value={input.image}
-          onBlur={handleChange}
-          onChange={handleChange}
-        />
-        {errors.image && <p>{errors.image}</p>}
-        <br />
+  const navigate = useNavigate();
 
-        <label htmlFor="hp">HP: </label>
-        <input
-          type="number"
-          name="hp"
-          value={input.hp}
-          onChange={handleChange}
-        />
-        {errors.hp && <p>{errors.hp}</p>}
-        <br />
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(postPokemon(input));
+    navigate('/home');
+  }
 
-        <label htmlFor="attack">Attack: </label>
-        <input
-          type="number"
-          name="attack"
-          value={input.attack}
-          onBlur={handleChange}
-          onChange={handleChange}
-        />
-        {errors.attack && <p>{errors.attack}</p>}
-        <br />
 
-        <label htmlFor="defense">Defense: </label>
-        <input
-          type="number"
-          name="defense"
-          value={input.defense}
-          onBlur={handleChange}
-          onChange={handleChange}
-        />
-        {errors.defense && <p>{errors.defense}</p>}
-        <br />
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Create your Pokémon!</h1>
 
-        <label htmlFor="types">Types: </label>
-        <select
-          name="types"
-          value={input.types}
-          onBlur={handleChange}
-          onChange={handleChange}
-        >
-          <option value=""></option>
-          {allTypes.map((type) => (
-            <option key={type.id} value={type.name}>
-              {type.name}
-            </option>
-          ))}
-        </select>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        name="name"
+        value={input.name}
+        onChange={handleChange}
+      />
+      {errors.name && <p>{errors.name}</p>}
+      <br></br><br></br>
 
-        {errors.types && <p>{errors.types}</p>}
-        <br />
+      <label htmlFor="image">Image URL: </label>
+      <input
+        type="text"
+        name="image"
+        value={input.image}
+        onChange={handleChange}
+      />
+      {errors.image && <p>{errors.image}</p>}
+      <br></br><br></br>
 
-        <Button type='submit' path='/home' text="Create Pokémon"></Button>
-      </form>
-    );
+      <label htmlFor="hp">HP: </label>
+      <input
+        type="text"
+        name="hp"
+        value={input.hp}
+        onChange={handleChange}
+      />
+      {errors.hp && <p>{errors.hp}</p>}
+      <br></br><br></br>
+
+      <label htmlFor="attack">Attack: </label>
+      <input
+        type="text"
+        name="attack"
+        value={input.attack}
+        onChange={handleChange}
+      />
+      {errors.attack && <p>{errors.attack}</p>}
+      <br></br><br></br>
+
+      <label htmlFor="defense">Defense: </label>
+      <input
+        type="text"
+        name="defense"
+        value={input.defense}
+        onChange={handleChange}
+      />
+      {errors.defense && <p>{errors.defense}</p>}
+      <br></br><br></br>
+
+      <label htmlFor="types">Types: </label>
+      <select
+        name="types"
+        value={input.types}
+        onChange={handleChange}
+      >
+        <option value=""></option>
+        {allTypes.map((type) => (
+          <option key={type.id} value={type.name}>
+            {type.name}
+          </option>
+        ))}
+      </select>
+
+      {errors.types && <p>{errors.types}</p>}
+      <br></br><br></br>
+
+      <button 
+        type='submit'
+        disabled={!input.name || errors.name || errors.image || errors.hp || errors.attack || errors.defense || errors.types}
+      >
+      Create Pokémon
+      </button>
+    </form>
+  );
 }
 
-export default Create
+export default Create;
