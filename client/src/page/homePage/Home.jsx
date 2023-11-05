37a0edfb10c38d5d006b4ba81752eb158dbfc0db
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-05 11:23:43 -0300

    Warning corrected: deleted paths, rendering of `Cards` in `Home`

--- client/src/page/homePage/Home.jsx
+++ client/src/page/homePage/Home.jsx
@@ -1,13 +1,10 @@
 import './Home.css';
-import { Routes, Route } from 'react-router-dom';
 import Cards from '../../components/cards/Cards';
 
 const Home = () => {
   return (
     <div className='Home'>
-      <Routes>
-        <Route path='' element={<Cards/>}></Route>
-      </Routes>
+        <Cards/>
     </div>
   );
 }
