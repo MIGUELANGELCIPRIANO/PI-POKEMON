f3ec328881cb114ca9323bedbb2893aa5b033fcd
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-06 11:45:10 -0300

    postPokemon, searchPokemon, pokemonOrder, filterPokemonBySource and filterPokemonByType added to `actions`

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,4 @@
-import { GET_ALL_POKEMONS, GET_POKEMON_DETAIL, CLEAN_POKEMON_DETAIL, GET_ALL_TYPES } from "./actionsTypes"
+import { GET_ALL_POKEMONS, GET_ALL_TYPES, GET_POKEMON_DETAIL, CLEAN_POKEMON_DETAIL, SEARCH_POKEMON, POKEMON_ORDER, FILTER_POKEMON_BY_SOURCE, FILTER_POKEMON_BY_TYPE } from "./actionsTypes"
 import axios from "axios";
 
 const URLPokemon = `http://localhost:3001/pokemons`;
@@ -10,6 +10,18 @@ export const getAllPokemons = () => {
             const response = await axios(`${URLPokemon}`);
             const data = response.data;
             dispatch({ type: GET_ALL_POKEMONS, payload: data });
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+}
+
+export const getAllTypes = () => {
+    return async (dispatch) => {
+        try {
+            const response = await axios(`${URLTypes}`);
+            const data = response.data;
+            dispatch({ type: GET_ALL_TYPES, payload: data });
         } catch (error) {
             throw new Error(error.message);
         }
@@ -32,14 +44,37 @@ export const cleanPokemonDetail = () => {
     return { type: CLEAN_POKEMON_DETAIL }
 }
 
-export const getAllTypes = () => {
-    return async (dispatch) => {
+export const postPokemon = (input) => {
+    return async () => {
         try {
-            const response = await axios(`${URLTypes}`);
-            const data = response.data;
-            dispatch({ type: GET_ALL_TYPES, payload: data });
+            const { data } = await axios.post(URLPokemon, input);
+            return data;
         } catch (error) {
             throw new Error(error.message);
         }
     }
+}
+
+export const searchPokemon = (name) => {
+    return async (dispatch) => {
+        try {
+            const response = await axios(`${URLPokemon}/name?name=${name}`);
+            const data = response.data;
+            dispatch({ type: SEARCH_POKEMON, payload: data });
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+}
+
+export const pokemonOrder = (order) => {
+    return { type: POKEMON_ORDER, payload: order }
+}
+
+export const filterPokemonBySource = (source) => {
+    return { type: FILTER_POKEMON_BY_SOURCE, payload: source }
+}
+
+export const filterPokemonByType = (types) => {
+    return { type: FILTER_POKEMON_BY_TYPE, payload: types }
 }
