f1498e8a76e3145fbade925650e2c1648b24f224
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-07 19:36:16 -0300

    dispatch(getAllPokemons()) added to `Home` linkbutton

--- client/src/components/button/Button.jsx
+++ client/src/components/button/Button.jsx
@@ -1,10 +1,17 @@
 import './Button.css';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getAllPokemons } from '../../redux/actions';
 
 const Button = ({ path, text }) => {
+    const dispatch = useDispatch();
     return (
         <Link to={path}>
-            <button>{text}</button>
+            <button onClick={() => {
+                if(path === '/home'){
+                    dispatch(getAllPokemons());
+                }
+            }}>{text}</button>
         </Link>
     );
 }
