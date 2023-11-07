ff26a63a866b8ea145020f4e457b0895b73362f9
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-07 19:29:04 -0300

    `Pagination` added to page folder

--- client/src/page/pagination/Pagination.jsx
+++ client/src/page/pagination/Pagination.jsx
@@ -1,20 +1,18 @@
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     return (
-        <div className={style.container}>
+        <div>
             <button
                 onClick={() => onPageChange(currentPage - 1)}
                 disabled={currentPage === 1}
-                className={style.button}
             >
                 &#8249;
             </button>
-            <span className={style.text}>
+            <span>
                 {currentPage} de {totalPages}
             </span>
             <button
                 onClick={() => onPageChange(currentPage + 1)}
                 disabled={currentPage === totalPages}
-                className={style.button}
             >
                 &#8250;
             </button>
@@ -23,45 +21,3 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 };
 
 export default Pagination;
-
-// CARDS / HOME ?
-//? import Pagination from '../../page/pagination/Pagination';
-
-// cosnt Cards / Home = ({ currentPage, setCurrentPage }) => {
-    
-// const PokesPerPage = 12;
-
-// const indexOfLastPokes = currentPage * PokesPerPage;
-// const indexOfFirstPokes = indexOfLastPokes - PokesPerPage;
-// const currentPokes = allPokemons ? allPokemons.slice(indexOfFirstPokes, indexOfLastPokes) : [];
-
-// const paginate = (pageNumber) => {
-//   setCurrentPage(pageNumber);
-// };
-
-//return ( 
-          {/* {allPokemons.length>0 ? <Pagination
-          currentPage={currentPage}
-          totalPages={Math.ceil(allPokemons.length / PokesPerPage)}
-          onPageChange={paginate}
-         />: null} */}
-// )
-// }
-
-// APP ?
-//? import { useState } from 'react';
-
-//const App = () => {
-//? const [currentPage, setCurrentPage] = useState(1);
-// return (
-//     <div className="App">
-//       {location.pathname !=='/' ? <NavBar/> : ''}
-//       <Routes>
-//         <Route path='' element={<Landing/>}></Route>
-//?         <Route path='/home' element={<Home currentPage={currentPage} setCurrentPage={setCurrentPage}/>}></Route>
-//         <Route path='/detail/:id' element={<Detail/>}></Route>
-//         <Route path='/form' element={<Create/>}></Route>
-//       </Routes>
-//     </div>
-//   );
-// }
