7f80eb5934a15772eeb4bfb5329eef4532a6157b
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-07 20:04:49 -0300

    Paging added to `Home`

--- client/src/page/homePage/Home.jsx
+++ client/src/page/homePage/Home.jsx
@@ -1,12 +1,15 @@
 import './Home.css';
+import { useState } from 'react';
 import Cards from '../../components/cards/Cards';
 import SearchBar from '../../components/searchBar/SearchBar';
 
+
 const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className='Home'>
       <SearchBar/>
-      <Cards/>
+      <Cards currentPage={currentPage} setCurrentPage={setCurrentPage}/>
     </div>
   );
 }
