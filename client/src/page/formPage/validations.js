4a15e51bfda3073cfb6372a16dcb169073cae1fb
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-07 13:56:51 -0300

    Modification of `validations` for types

--- client/src/page/formPage/validations.js
+++ client/src/page/formPage/validations.js
@@ -6,7 +6,7 @@ const validations = (input) => {
     if (input.hp === '' || isNaN(input.hp) || input.hp <= 0 || input.hp > 255) { errors.hp = "HP must be a number between 1 and 255" }
     if (input.attack === '' || isNaN(input.attack) || input.attack <= 4 || input.attack > 210) { errors.attack = "Attack must be a number between 5 and 210" }
     if (input.defense === '' || isNaN(input.defense) || input.defense <= 4 || input.defense > 230) { errors.defense = "Defense must be a number between 5 and 230" }
-    if (input.types.length === 0 || input.types.length > 2) { errors.types = "You must select between 1 and 2 types" }
+    if (input.types.length === 0 || input.types.length >= 2) { errors.types = "You must select between 1 and 2 types" }
 
     return errors;
 }
