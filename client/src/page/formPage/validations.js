17d03473599e493efe705df6405ad0618f4d1932
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-09 01:05:09 -0300

    Validation of input.name in lower case and without numeric characters in `validations`

--- client/src/page/formPage/validations.js
+++ client/src/page/formPage/validations.js
@@ -1,7 +1,7 @@
 const validations = (input) => {
     const errors = {};
 
-    if (input.name === '' || input.name.length < 2 || input.name.length >= 12) { errors.name = "The Pokémon's name must be between 2 and 13 characters" }
+    if (!/^[a-z]+$/.test(input.name) || input.name.length < 2 || input.name.length >= 12) { errors.name = "The Pokémon's name must contain only lowercase letters and be between 2 and 13 characters" }
     if (!(input.image.startsWith('http://') || input.image.startsWith('https://'))) { errors.image = "The image must be a URL" }
     if (input.hp === '' || isNaN(input.hp) || input.hp <= 0 || input.hp > 255) { errors.hp = "HP must be a number between 1 and 255" }
     if (input.attack === '' || isNaN(input.attack) || input.attack <= 4 || input.attack > 210) { errors.attack = "Attack must be a number between 5 and 210" }
